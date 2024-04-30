0eaf2b4defe8fa183f6b688fbb1f741019cd8c35
vaibhavverulkar08 <vaibhavverulkar08@gmail.com>
2024-04-30 18:48:18 +0530

    My SeleniumPractice Selenium v4 2k24

---
+++ MySeleniumJavaPractice/src/myscript/webdriver/WebElementCommands.java
@@ -0,0 +1,107 @@
+package myscript.webdriver;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+// Selenium WebDriver - WebElement Commands
+//Test Automation Scenario : Verify valid user Gmail SigIn page.
+// Automated By Vaibhav Verulkar
+
+public class WebElementCommands {
+
+	WebDriver driver;
+	String url = "https://www.google.com/";
+	String userEmail = "";
+	// String xpathSigninbtn = //*[@id="gb"]/div/div[2]/a/span
+	// xpath Nextbtn = //*[@id="identifierNext"]/div/button/span
+
+	public static void main(String[] args) throws Exception {
+		WebElementCommands ts = new WebElementCommands();
+		// Steps:
+		ts.launchBrowser();
+		System.out.println("Browser Launched");
+		ts.openURL();
+		System.out.println("URl Opened");
+
+		ts.clickOnSignInBtn();
+		System.out.println("SignIn Button Clicked");
+
+		ts.enterEmail();
+		System.out.println("Email Entered");
+
+		ts.clickNext();
+		System.out.println("Clicked on NEXT");
+
+		System.out.println("---Printing Message on Console:---");
+		ts.getScreenMessage();
+
+		System.out.println("");
+
+	}
+
+	public void launchBrowser() {
+		// System Property for Chrome Driver
+		System.setProperty("webdriver.chrome.driver", "browser-drivers/chromedriver-win64/chromedriver.exe");
+		// Instantiate a ChromeDriver class.
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+
+		// imlicitly wait
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	}
+
+	public void getpageDetails() {
+		System.out.println("Curent Page URL: " + driver.getCurrentUrl());
+		System.out.println("Curent Page Title: " + driver.getTitle());
+	}
+
+	public void openURL() {
+		driver.get(url);
+		getpageDetails();
+	}
+
+	public void clickOnSignInBtn() {
+		WebElement sigInbtn = driver.findElement(By.xpath("//*[@id=\"gb\"]/div/div[2]/a/span"));
+		sigInbtn.click();
+	}
+
+	public void enterEmail() {
+		// WebElement inputEmail = driver.findElement(By.id("identifierId"));
+		// inputEmail.clear();
+		// inputEmail.sendKeys(userEmail);
+
+		// OR
+		WebElement emailInputField = driver.findElement(By.id("identifierId"));
+		boolean status = emailInputField.isEnabled();
+		// Check that if the Text field is enabled, if yes enter value
+		System.out.println(status);
+		if (status) {
+			emailInputField.sendKeys(userEmail);
+		}
+	}
+
+	private void clickNext() throws Exception {
+		WebElement btnNext = driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/div/button/span"));
+		btnNext.click();
+		Thread.sleep(5000);
+		//getpageDetails();
+	}
+	
+	public void getScreenMessage() {
+		//WebElement fetchText = driver.findElement(By.xpath("//*[@id='yDmH0d']/c-wiz/div/div[2]"));
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+		WebElement fetchText=wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='yDmH0d']/c-wiz/div/div[2]")));
+		
+		String message =fetchText.getText();
+		System.out.println("The Message is on Screen: "+message);
+	}
+	
+	public void tearDown() {
+		driver.quit();
+	}
+}
